ea47c6089a0bc323bb9f6148202ca40db0806a9b
Lispehol <146867390+Lispehol@users.noreply.github.com>
2024-08-26 19:25:16 +0300

    Add css-file and some updates

--- insertData.php
+++ insertData.php
@@ -1,29 +1,25 @@
 <?php
+    require('settings2.php');
 
-require('settings2.php');
-
-//create connetion
-$conn = new mysqli($servername, $username, $password, $dbname);
-//check connection
-if(!$conn) {
-    die('Failed to connect to server: ' . mysqli_error());
-}
-
-//tuodaan tieto muuttujasta päätiedostosta
-$txt = $_GET['txt'];
-$name = $_GET['name'];
-
-//txt = $_POST['txt'];
-//$name = $_POST['name'];
-$time = date("Y-m-d h:i:s");
-//sql lauseella tietokantaan vienti
-$sql = "INSERT INTO chat2 (nimi, viesti, pvm) values ('$name', '$txt', '$time')";
-
-if($conn->query($sql) == TRUE){
-    echo "Viesti lähetetty.";
-}
-    else {
-        echo "Error: " .sql. "<br>" . $conn->error;
+    //create connetion
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //check connection
+    if(!$conn) {
+        die('Failed to connect to server: ' . mysqli_error());
     }
-$conn->close();
+
+    //data is imported from main file
+    $txt = $_GET['txt'];
+    $name = $_GET['name'];
+
+    $time = date("Y-m-d h:i:s");
+    //data is exported to the database with sql
+    $sql = "INSERT INTO chat2 (nimi, viesti, pvm) values ('$name', '$txt', '$time')";
+
+    if($conn->query($sql) == TRUE){
+        echo "Viesti lähetetty.";
+    } else {
+            echo "Error: " .sql. "<br>" . $conn->error;
+        }
+    $conn->close();
 ?>
